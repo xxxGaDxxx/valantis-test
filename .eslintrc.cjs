4f49706cbe977de3e66a2b743c5ebacbdc0b7ac5
Wlad loban <qwegadqwe@gmail.com>
2024-03-02 15:48:31 +0300

    #1 initial

---
+++ .eslintrc.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ['@it-incubator/eslint-config'],
+  rules: {
+    'no-console': ['warn', {
+      allow: ['warn', 'error', 'info']
+    }]
+  }
+};
---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react'
+
+import { createRoot } from 'react-dom/client'
+
+import './styles/index.scss'
+
+import { App } from './app'
+
+createRoot(document.getElementById('root') as HTMLElement).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
